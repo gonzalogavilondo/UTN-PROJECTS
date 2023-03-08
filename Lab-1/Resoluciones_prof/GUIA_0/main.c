7baf92e3ce038347cf2b7fedf13e5e8782ce9678
DESKTOP-0D8PHQM\Gonza <gonzalogavilondo@gmail.com>
2023-03-08 13:35:02 -0300

    no message

---
+++ Lab-1/Resoluciones_prof/GUIA_0/main.c
@@ -0,0 +1,18 @@
+#include "declaraciones.h"
+
+int main()
+{
+    char control;
+    do
+    {
+        int opcion = menu();
+        ejecutarEjercicio(opcion);
+        system("pause");
+        system("cls || clean");
+        printf("Desea probar otro ejercicio? Presione s/n <si/cualquier otra letra>: ");
+        fflush(stdin);
+        scanf("%c", &control);
+    }while (control == 's' || control == 'S');
+
+    return 0;
+}
