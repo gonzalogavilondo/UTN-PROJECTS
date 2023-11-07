1256ee4a88781a46e9af55f4c3128a917bf9af00
DESKTOP-NHC40B6\Gonzalo <gonzalogavilondo@gmail.com>
2023-11-07 09:49:58 -0300

    test

--- Lab-2/2DO_PARCIAL_PRACTICA_2/ResolucionP2020/main.c
+++ Lab-2/2DO_PARCIAL_PRACTICA_2/ResolucionP2020/main.c
@@ -19,13 +19,13 @@ int main()
     int idEspecialidadBusqueda;
 
     do {
-        printf("\n*** Menú de opciones ***\n");
-        printf("1. Agregar nueva atención médica\n");
+        printf("\n*** Menu de opciones ***\n");
+        printf("1. Agregar nueva atencion medica\n");
         printf("2. Buscar si un paciente fue atendido en una especialidad determinada\n");
-        printf("3. Determinar qué especialidad médica tuvo más atenciones\n");
-        printf("4. Realizar la persistencia de las atenciones médicas de una especialidad médica en particular\n");
+        printf("3. Determinar que especialidad medica tuvo mas atenciones\n");
+        printf("4. Realizar la persistencia de las atenciones medicas de una especialidad medica en particular\n");
         printf("5. Salir\n");
-        printf("Ingrese la opción deseada: ");
+        printf("Ingrese la opcion deseada: ");
         scanf("%d", &opcion);
 
         switch (opcion)
@@ -44,16 +44,16 @@ int main()
                 fgets(apellidoPaciente, sizeof(apellidoPaciente), stdin);
                 apellidoPaciente[strcspn(apellidoPaciente, "\n")] = '\0'; // Elimina el salto de línea
 
-                printf("Ingrese el ID de la especialidad médica: ");
+                printf("Ingrese el ID de la especialidad medica: ");
                 scanf("%d", &idEspecialidadBusqueda);
                 int encontrado = buscarPacienteEnEspecialidad(arreglo, validos, nombrePaciente, apellidoPaciente, idEspecialidadBusqueda);
                 if (encontrado)
                 {
-                    printf("El paciente fue atendido en la especialidad médica especificada.\n");
+                    printf("El paciente fue atendido en la especialidad medica especificada.\n");
                 }
                 else
                 {
-                    printf("El paciente no fue atendido en la especialidad médica especificada.\n");
+                    printf("El paciente no fue atendido en la especialidad medica especificada.\n");
                 }
                 break;
 
@@ -63,7 +63,7 @@ int main()
                 break;
             case 4:
                 // Ejercicio 7: Persistencia de atenciones de una especialidad
-                printf("Ingrese el nombre de la especialidad médica: ");
+                printf("Ingrese el nombre de la especialidad medica: ");
                 scanf("%s", nombreEspecialidad);
                 printf("Ingrese el nombre del archivo de salida: ");
                 scanf("%s", nombreArchivo);
@@ -73,7 +73,7 @@ int main()
                 printf("Saliendo del programa...\n");
                 break;
             default:
-                printf("Opción no válida. Por favor, elija una opción válida.\n");
+                printf("Opcion no valida. Por favor, elija una opcion valida.\n");
         }
         system("pause");
 
