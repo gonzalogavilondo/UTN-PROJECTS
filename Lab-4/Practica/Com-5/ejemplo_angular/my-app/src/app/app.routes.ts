73e6136289b75d6919aed3df5812f26421247538
GONZALO\gonza <gonzalogavilondo@gmail.com>
2026-09-14 20:57:24 -0300

    Test Angular

--- Lab-4/Practica/Com-5/ejemplo_angular/my-app/src/app/app.routes.ts
+++ Lab-4/Practica/Com-5/ejemplo_angular/my-app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'miruta', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)},
+  {path: '', redirectTo: 'home'},
+];
